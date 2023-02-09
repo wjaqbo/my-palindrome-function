d4540bc522f0184a06cbc23c0e8b36143cccf53e
Wiktor Jakubowski <qbagency@gmail.com>
2023-02-08 21:12:45 -0600

    Refactor

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,14 @@
-function checkIfWordIsPalindrome(word) {}
+function checkIfWordIsPalindrome(word) {
+  const isEmpty = () => word === undefined || word === null || word === '';
+  const isNotString = () => typeof word !== 'string';
+  const isNotPalindrome = () => {
+    const orginalWord = word.toLowerCase();
+    const reversedWord = orginalWord.split('').reverse().join('');
+    return orginalWord !== reversedWord || word.length === 1;
+  };
+
+  if (isEmpty()) return 'Word is required';
+  if (isNotString()) return 'Word has to be a string';
+  if (isNotPalindrome()) return 'Word is not Palindrome';
+  return 'Word is Palindrome!';
+}
